8c4a54494d13f2cb5e6d594268869d78665afcaf
TheGreyDiamond <s-einkauf@oest-rs.de>
2022-01-05 14:51:20 +0100

    Fixed playbar + better minify

---
+++ src/dropHandler.js
@@ -0,0 +1,30 @@
+document.addEventListener("drop", (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  document.getElementById("fakeDropzone").style.display = "none";
+  document.getElementById("loadingBig").style.display = "block";
+  document.getElementById("state").innerHTML =
+    "Loading file. If this stays empty try another file.";
+  const superFile = event.dataTransfer.files[0].path;
+  FileType.fromFile(superFile).then((type) => {
+    type = type["mime"].split("/")[0];
+    if (type == "image") {
+      loadImageFromSource(superFile);
+    } else if (type == "video") {
+      loadVideoFromSource(superFile);
+    }
+  });
+});
+
+document.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
+document.addEventListener("dragenter", (event) => {
+  console.log("File is in the Drop Space");
+});
+
+document.addEventListener("dragleave", (event) => {
+  console.log("File has left the Drop Space");
+});
---
+++ tooling/minifyAll/minifyAllSrcs.js
@@ -0,0 +1,36 @@
+import {minify} from 'minify';
+import tryToCatch from 'try-to-catch';
+import fs from 'fs';
+import glob from 'glob';
+
+const options = {
+    
+};
+
+
+var getDirectories = function (src, callback) {
+    glob('src/**/*', callback);
+  };
+  getDirectories('test', function (err, res) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+        for(const file in res){
+            // console.log(minify(res[file], options));
+            const myFile = res[file];
+            const dst = myFile.replace("src", "dist");
+            console.log(myFile, "    ->    ", dst);
+            if(!fs.lstatSync(myFile).isDirectory() ){
+                let data = minify(myFile, options).then(function(data) {
+                    fs.writeFileSync(dst, data);
+                })
+                
+                //
+            }
+            
+            
+            //
+        }
+      
+    }
+  });
